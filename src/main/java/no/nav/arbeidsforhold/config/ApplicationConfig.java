027b03bc588ca70ef529aa8520b2354b80a42dcd
Hein Haraldsen <hein.haraldsen@nav.no>
2019-05-31 09:58:29 +0200

    IN-765 Endret ComponentScan igjen

--- src/main/java/no/nav/arbeidsforhold/config/ApplicationConfig.java
+++ src/main/java/no/nav/arbeidsforhold/config/ApplicationConfig.java
@@ -26,7 +26,7 @@ import java.net.URL;
 import java.util.EnumSet;
 
 @SpringBootConfiguration
-@ComponentScan({"no.nav.arbeidsforhold.services", "no.nav.arbeidsforhold.api"})
+@ComponentScan({"no.nav.arbeidsforhold","no.nav.kodeverk"})
 @EnableConfigurationProperties(MultiIssuerProperties.class)
 @Import({RestClientConfiguration.class,
 })
