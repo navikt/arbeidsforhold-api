c8b3805e70317d58d1af221c7fb34d2dafef5c43
Øyvind Hagen <oyvind.hagen@nav.no>
2019-08-30 09:44:48 +0200

    Fjernet ubrukte imports.

--- src/main/java/no/nav/arbeidsforhold/config/ArbeidsforholdRestConfiguration.java
+++ src/main/java/no/nav/arbeidsforhold/config/ArbeidsforholdRestConfiguration.java
@@ -3,7 +3,6 @@ package no.nav.arbeidsforhold.config;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import no.nav.security.oidc.jaxrs.OidcClientRequestFilter;
 import org.glassfish.jersey.client.ClientProperties;
-import org.glassfish.jersey.logging.LoggingFeature;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -15,8 +14,6 @@ import javax.ws.rs.client.ClientRequestFilter;
 import javax.ws.rs.ext.ContextResolver;
 import java.net.URI;
 import java.net.URISyntaxException;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 @Configuration
 public class ArbeidsforholdRestConfiguration {
