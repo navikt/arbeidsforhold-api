4fc05f763597af8fbfbe5ba18cc097357757beb1
Øyvind Hagen <oyvind.hagen@nav.no>
2019-12-16 10:10:06 +0100

    Ekskluderer sporingsinformasjon fra spørring på tjeneste.

--- src/main/java/no/nav/arbeidsforhold/config/ArbeidsforholdConsumer.java
+++ src/main/java/no/nav/arbeidsforhold/config/ArbeidsforholdConsumer.java
@@ -3,10 +3,7 @@ package no.nav.arbeidsforhold.config;
 import no.nav.arbeidsforhold.domain.Arbeidsforhold;
 import no.nav.arbeidsforhold.exceptions.ArbeidsforholdConsumerException;
 import no.nav.log.MDCConstants;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.slf4j.MDC;
-
 
 import javax.ws.rs.ProcessingException;
 import javax.ws.rs.client.Client;
@@ -25,32 +22,28 @@ public class ArbeidsforholdConsumer {
     private static final String REGELVERK = "A_ORDNINGEN";
     private Client client;
     private URI endpoint;
-    private static final Logger log = LoggerFactory.getLogger(ArbeidsforholdConsumer.class);
-
 
     public ArbeidsforholdConsumer(Client client, URI endpoint) {
         this.client = client;
         this.endpoint = endpoint;
     }
 
-
     public List<Arbeidsforhold> hentArbeidsforholdmedFnr(String fnr, String fssToken) {
         Invocation.Builder request = buildFnrRequest(fnr, fssToken);
         return hentArbeidsforholdmedFnr(request);
     }
-
 
     public Arbeidsforhold hentArbeidsforholdmedId(String fnr, int id, String fssToken) {
         Invocation.Builder request = buildForholdIdRequest(fnr, id, fssToken);
         return hentArbeidsforholdmedId(request);
     }
 
-
     private Invocation.Builder buildFnrRequest(String fnr, String fssToken) {
 
         return client.target(endpoint)
                 .path("v1/arbeidstaker/arbeidsforhold")
                 .queryParam("regelverk", REGELVERK)
+                .queryParam("sporingsinformasjon", false)
                 .request()
                 .header("Nav-Call-Id", MDC.get(MDCConstants.MDC_CALL_ID))
                 .header("Nav-Consumer-Id", CONSUMER_ID)
@@ -63,13 +56,13 @@ public class ArbeidsforholdConsumer {
         return client.target(endpoint)
                 .path("v1/arbeidsforhold/" + id)
                 .queryParam("historikk", true)
+                .queryParam("sporingsinformasjon", false)
                 .request()
                 .header("Nav-Call-Id", MDC.get(MDCConstants.MDC_CALL_ID))
                 .header("Nav-Consumer-Id", CONSUMER_ID)
                 .header("Nav-Consumer-Token", BEARER.concat(fssToken))
                 .header("Nav-Personident", fnr);
     }
-
 
     private List<Arbeidsforhold> hentArbeidsforholdmedFnr(Invocation.Builder request) {
         try (Response response = request.get()) {
@@ -93,13 +86,11 @@ public class ArbeidsforholdConsumer {
         }
     }
 
-
     private List<Arbeidsforhold> readFnrResponse(Response r) {
         if (!SUCCESSFUL.equals(r.getStatusInfo().getFamily())) {
             String msg = "Forsøkte å konsumere REST-tjenesten Arbeidsforhold. endpoint=[" + endpoint + "], HTTP response status=[" + r.getStatus() + "].";
             throw new ArbeidsforholdConsumerException(msg + " - " + readEntity(String.class, r));
         } else {
-            //log.warn("Arbeidsforhold: " + r.readEntity(String.class));
             List arbeidsforholdList = r.readEntity(new GenericType<List<Arbeidsforhold>>() {
             });
             return arbeidsforholdList;
@@ -111,7 +102,6 @@ public class ArbeidsforholdConsumer {
             String msg = "Forsøkte å konsumere REST-tjenesten Arbeidsforhold. endpoint=[" + endpoint + "], HTTP response status=[" + r.getStatus() + "].";
             throw new ArbeidsforholdConsumerException(msg + " - " + readEntity(String.class, r));
         } else {
-            //log.warn("Arbeidsforholdinnslag: " + r.readEntity(String.class));
             Arbeidsforhold arbeidsforhold = r.readEntity(Arbeidsforhold.class);
             return arbeidsforhold;
         }
