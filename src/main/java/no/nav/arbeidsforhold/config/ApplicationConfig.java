be78c9971bbb855f5d3b7d475f4a4de5844be0f7
Morten Jansrud <morten.jansrud@nav.no>
2020-10-26 12:53:40 +0100

    copy config

--- src/main/java/no/nav/arbeidsforhold/config/ApplicationConfig.java
+++ src/main/java/no/nav/arbeidsforhold/config/ApplicationConfig.java
@@ -1,6 +1,5 @@
 package no.nav.arbeidsforhold.config;
 
-import io.prometheus.client.hotspot.DefaultExports;
 import no.nav.log.LogFilter;
 import no.nav.security.token.support.core.configuration.MultiIssuerConfiguration;
 import no.nav.security.token.support.core.configuration.ProxyAwareResourceRetriever;
@@ -12,7 +11,9 @@ import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.boot.web.embedded.jetty.JettyServletWebServerFactory;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.boot.web.servlet.filter.OrderedRequestContextFilter;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.context.EnvironmentAware;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
@@ -20,6 +21,7 @@ import org.springframework.context.annotation.Import;
 import org.springframework.core.Ordered;
 import org.springframework.core.env.Environment;
 import org.springframework.web.context.request.RequestContextListener;
+import org.springframework.web.filter.RequestContextFilter;
 
 import javax.servlet.DispatcherType;
 import java.net.InetSocketAddress;
@@ -35,17 +37,20 @@ import java.util.EnumSet;
         "no.nav.arbeidsforhold.config",
         "no.nav.arbeidsforhold.tasks"})
 @EnableConfigurationProperties(MultiIssuerProperties.class)
-@Import({RestClientConfiguration.class,
-})
+@Cacheable
+@Import({RestClientConfiguration.class})
 public class ApplicationConfig implements EnvironmentAware {
-
-    static  {
-        DefaultExports.initialize();
-    }
 
     private static final Logger log = LoggerFactory.getLogger(ApplicationConfig.class);
 
     private Environment env;
+
+    @Bean
+    public RequestContextFilter requestContextFilter() {
+        OrderedRequestContextFilter filter = new OrderedRequestContextFilter();
+        filter.setOrder(Ordered.HIGHEST_PRECEDENCE + 1);
+        return filter;
+    }
 
     @Bean
     ServletWebServerFactory servletWebServerFactory() {
@@ -101,7 +106,10 @@ public class ApplicationConfig implements EnvironmentAware {
         log.info("Registering LogFilter filter");
         final FilterRegistrationBean<LogFilter> filterRegistration = new FilterRegistrationBean<>();
         filterRegistration.setFilter(new LogFilter());
-        filterRegistration.setOrder(1);
+
+        // Viktig at order settes til en lavere verdi enn hva jersey-filteret er konfigurert med i application-yaml
+        // slik at loggfilteret kjøres først. Årsaken til dette er avhengigheter til MDC i forretningskoden (uthenting av callId).
+        filterRegistration.setOrder(-100001);
         return filterRegistration;
     }
 
