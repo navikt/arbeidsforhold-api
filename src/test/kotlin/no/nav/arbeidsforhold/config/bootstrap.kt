66073b33a2f843f5ffea3f83f05488b04c782964
Bjørnar Hunshamar <bjornar.hunshamar@nav.no>
2022-08-26 13:44:59 +0200

    Henter token fra auth-header dersom cookie ikke er satt. Fikset logging og mocking av util-metoder ved lokal kjøring.

--- src/test/kotlin/no/nav/arbeidsforhold/config/bootstrap.kt
+++ src/test/kotlin/no/nav/arbeidsforhold/config/bootstrap.kt
@@ -3,7 +3,10 @@ package no.nav.arbeidsforhold.config
 import io.ktor.serialization.kotlinx.json.json
 import io.ktor.server.application.Application
 import io.ktor.server.application.install
+import io.ktor.server.plugins.callloging.CallLogging
 import io.ktor.server.plugins.contentnegotiation.ContentNegotiation
+import io.ktor.server.request.httpMethod
+import io.ktor.server.request.path
 import io.ktor.server.routing.routing
 import no.nav.arbeidsforhold.routes.arbeidsforholdFnr
 import no.nav.arbeidsforhold.routes.arbeidsforholdId
@@ -13,6 +16,16 @@ fun Application.testModule(appContext: TestApplicationContext) {
 
     install(ContentNegotiation) {
         json(jsonConfig())
+    }
+
+    install(CallLogging) {
+        filter { call -> !call.request.path().contains("internal") }
+        format { call ->
+            val status = call.response.status()
+            val httpMethod = call.request.httpMethod.value
+            val path = call.request.path()
+            "$status - $httpMethod $path"
+        }
     }
 
     routing {
--- src/test/kotlin/no/nav/arbeidsforhold/localServer.kt
+++ src/test/kotlin/no/nav/arbeidsforhold/localServer.kt
@@ -11,13 +11,13 @@ import no.nav.arbeidsforhold.util.getFnrFromToken
 import no.nav.arbeidsforhold.util.getSelvbetjeningTokenFromCall
 
 fun main() {
-    mockkStatic(::getSelvbetjeningTokenFromCall)
-    coEvery { getSelvbetjeningTokenFromCall(any()) } returns "dummyToken"
-
-    mockkStatic(::getFnrFromToken)
-    coEvery { getFnrFromToken(any()) } returns "10108000398"
-
     embeddedServer(Netty, port = 8080, watchPaths = listOf("classes")) {
+        mockkStatic(::getSelvbetjeningTokenFromCall)
+        mockkStatic(::getFnrFromToken)
+
+        coEvery { getSelvbetjeningTokenFromCall(any()) } returns "dummyToken"
+        coEvery { getFnrFromToken(any()) } returns "10108000398"
+
         testModule(TestApplicationContext(setupMockedClient()))
     }.start(wait = true)
 }
