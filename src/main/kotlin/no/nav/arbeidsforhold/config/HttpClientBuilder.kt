7a511a32394a29a5440d7eb39886fb1c78bdf3e3
Bjørnar Hunshamar <bjornar.hunshamar@nav.no>
2023-03-16 15:10:11 +0100

    Fikset timeout-retry

--- src/main/kotlin/no/nav/arbeidsforhold/config/HttpClientBuilder.kt
+++ src/main/kotlin/no/nav/arbeidsforhold/config/HttpClientBuilder.kt
@@ -3,10 +3,9 @@ package no.nav.arbeidsforhold.config
 import io.ktor.client.HttpClient
 import io.ktor.client.engine.apache.Apache
 import io.ktor.client.plugins.HttpRequestRetry
-import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
+import io.ktor.client.plugins.HttpTimeout
 import io.ktor.client.plugins.defaultRequest
 import io.ktor.client.request.header
-import io.ktor.serialization.kotlinx.json.json
 
 object HttpClientBuilder {
 
@@ -15,12 +14,14 @@ object HttpClientBuilder {
             defaultRequest {
                 header("Nav-Consumer-Id", CONSUMER_ID)
             }
-            install(ContentNegotiation) {
-                json(jsonConfig())
-            }
             install(HttpRequestRetry) {
-                retryOnExceptionOrServerErrors(maxRetries = 3)
-                constantDelay(3000L)
+                retryOnServerErrors(maxRetries = 3)
+                retryOnException(maxRetries = 3, retryOnTimeout = true)
+                constantDelay(1000)
+            }
+            install(HttpTimeout) {
+                requestTimeoutMillis = 5000
+                connectTimeoutMillis = 2000
             }
             expectSuccess = false
         }
