ea18e7916aae82eafb55c23bac42f3b2af71b7f5
Bjørnar Hunshamar <bjornar.hunshamar@nav.no>
2024-05-23 13:47:51 +0200

    Bumps

--- build.gradle.kts
+++ build.gradle.kts
@@ -31,8 +31,8 @@ dependencies {
     val logstashVersion = "7.4"
     val navSecurityVersion = "4.1.7"
     val mockkVersion = "1.13.11"
-    val kotlinVersion = "1.9.22"
-    val tmsKtorTokenSupportVersion = "4.0.0"
+    val kotlinVersion = "2.0.0"
+    val tmsKtorTokenSupportVersion = "4.1.1"
 
     implementation("com.auth0:java-jwt:$auth0JwtVersion")
     implementation("io.ktor:ktor-server-netty:$ktorVersion")
@@ -51,7 +51,7 @@ dependencies {
     implementation("ch.qos.logback:logback-classic:$logbackVersion")
     implementation("net.logstash.logback:logstash-logback-encoder:$logstashVersion")
     implementation("no.nav.security:token-validation-ktor-v2:$navSecurityVersion")
-    implementation("com.github.navikt.tms-ktor-token-support:tokendings-exchange:$tmsKtorTokenSupportVersion")
+    implementation("no.nav.tms.token.support:tokendings-exchange:$tmsKtorTokenSupportVersion")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit5:$kotlinVersion")
     testImplementation("io.ktor:ktor-server-test-host:$ktorVersion")
     testImplementation("io.ktor:ktor-client-mock:$ktorVersion")
