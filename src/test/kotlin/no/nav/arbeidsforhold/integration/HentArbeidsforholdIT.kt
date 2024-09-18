6f948a0ae82e3afd421c0bc4ed8509c0ed436a27
Bjørnar Hunshamar <bjornar.hunshamar@nav.no>
2024-09-18 13:15:31 +0200

    Flyttet opprettelse av httpClient inn i get-metode i stedet for å kalle det eksplisitt i hver integrasjonstest

--- src/test/kotlin/no/nav/arbeidsforhold/integration/HentArbeidsforholdIT.kt
+++ src/test/kotlin/no/nav/arbeidsforhold/integration/HentArbeidsforholdIT.kt
@@ -2,10 +2,8 @@ package no.nav.arbeidsforhold.integration
 
 import io.kotest.assertions.json.shouldEqualJson
 import io.kotest.matchers.shouldBe
-import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
 import io.ktor.client.statement.bodyAsText
 import io.ktor.http.HttpStatusCode
-import io.ktor.serialization.kotlinx.json.json
 import no.nav.arbeidsforhold.config.setupMockedClient
 import no.nav.arbeidsforhold.testutils.readJsonFile
 import kotlin.test.Test
@@ -14,9 +12,7 @@ class HentArbeidsforholdIT : IntegrationTest() {
 
     @Test
     fun hentArbeidsforhold200() = integrationTest(setupMockedClient()) {
-        val client = createClient { install(ContentNegotiation) { json() } }
-
-        val response = get(client, HENT_ARBEIDSFORHOLD_FNR_PATH)
+        val response = get(HENT_ARBEIDSFORHOLD_FNR_PATH)
 
         response.status shouldBe HttpStatusCode.OK
         response.bodyAsText() shouldEqualJson readJsonFile("/json/expected-response/arbeidsforhold-list.json")
@@ -24,18 +20,14 @@ class HentArbeidsforholdIT : IntegrationTest() {
 
     @Test
     fun feilMotAaregSkalGi500() = integrationTest(setupMockedClient(aaregStatus = HttpStatusCode.InternalServerError)) {
-        val client = createClient { install(ContentNegotiation) { json() } }
-
-        val response = get(client, HENT_ARBEIDSFORHOLD_FNR_PATH)
+        val response = get(HENT_ARBEIDSFORHOLD_FNR_PATH)
 
         response.status shouldBe HttpStatusCode.InternalServerError
     }
 
     @Test
     fun feilMotEregSkalGi200() = integrationTest(setupMockedClient(eregStatus = HttpStatusCode.InternalServerError)) {
-        val client = createClient { install(ContentNegotiation) { json() } }
-
-        val response = get(client, HENT_ARBEIDSFORHOLD_FNR_PATH)
+        val response = get(HENT_ARBEIDSFORHOLD_FNR_PATH)
 
         response.status shouldBe HttpStatusCode.OK
         response.bodyAsText() shouldEqualJson readJsonFile("/json/expected-response/arbeidsforhold-list-with-ereg-error.json")
--- src/test/kotlin/no/nav/arbeidsforhold/integration/IntegrationTest.kt
+++ src/test/kotlin/no/nav/arbeidsforhold/integration/IntegrationTest.kt
@@ -3,9 +3,11 @@ package no.nav.arbeidsforhold.integration
 import com.auth0.jwt.JWT
 import com.auth0.jwt.algorithms.Algorithm
 import io.ktor.client.HttpClient
+import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
 import io.ktor.client.request.get
 import io.ktor.client.request.header
 import io.ktor.client.statement.HttpResponse
+import io.ktor.serialization.kotlinx.json.json
 import io.ktor.server.config.ApplicationConfig
 import io.ktor.server.testing.ApplicationTestBuilder
 import io.ktor.server.testing.testApplication
@@ -25,10 +27,10 @@ open class IntegrationTest {
         block()
     }
 
-    suspend fun get(client: HttpClient, path: String, setFnrHeader: Boolean = false): HttpResponse {
+    suspend fun ApplicationTestBuilder.get(path: String, setFnrHeader: Boolean = false): HttpResponse {
         val token = createAccessToken()
 
-        return client.get(path) {
+        return httpClient.get(path) {
             header("Authorization", "Bearer $token")
             if (setFnrHeader) {
                 header(FNR_ARBEIDSTAKER, FNR)
@@ -39,6 +41,11 @@ open class IntegrationTest {
     private fun createAccessToken(fnr: String = FNR): String {
         return JWT.create().withClaim("pid", fnr).sign(Algorithm.HMAC256("1"))
     }
+
+    private val ApplicationTestBuilder.httpClient
+        get() = createClient {
+            install(ContentNegotiation) { json() }
+        }
 
     companion object {
         private const val FNR = "12345678911"
